c218d1d778d0641bc0a6de7b0414ec6baae09b3e
karinabogacz <60329769+karinabogacz@users.noreply.github.com>
2020-03-30 20:25:58 +0200

    Add files via upload

--- application.py
+++ application.py
@@ -1,11 +1,12 @@
 import os
 
-from flask import Flask, session, render_template
+from flask import Flask, session, render_template, request, redirect, url_for
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 app = Flask(__name__)
+app.secret_key = "key"
 
 # Check for environment variable
 if not os.getenv("DATABASE_URL"):
@@ -25,6 +26,48 @@ db = scoped_session(sessionmaker(bind=engine))
 def index():
     return render_template ("index.html")
 
-@app.route("/sign_up")
+@app.route("/sign_up", methods=['GET','POST'])
 def sign_up():
+    if request.method == "POST":
+        username = request.form.get("username")
+        email = request.form.get("email")
+        password = str(request.form.get("password"))
+
+        if username == "" or email == "" or password == "":
+            return render_template ("error.html", message = "You need to fill in all fields")
+        try:
+            db.execute("INSERT INTO users (username, email, password) VALUES (:username, :email, :password)", {"username": username, "email": email, "password": password})
+            db.commit()
+            session["user"] = username
+            return redirect(url_for("search"))
+        except:
+            return render_template("error.html", message = "username taken")
     return render_template("sign_up.html")
+
+@app.route("/login", methods=['GET','POST'])
+def login():
+
+    if request.method == "POST":
+
+        username = request.form.get("username")
+        password = request.form.get("password")
+        
+        if (db.execute("SELECT * FROM users WHERE username = :username AND password = :password", {"username":username, "password": password}).rowcount == 0):
+            return render_template("error.html", message = "Wrong username or password")
+        else:
+            session["user"] = username
+            return redirect(url_for("search"))
+    else:
+        return render_template ("login.html")
+
+@app.route("/search")
+def search():
+    if "user" in session:
+        return render_template("search.html", user=session["user"] ) 
+    else:
+        return redirect(url_for("login"))
+
+@app.route ("/logout")
+def logout():
+    session.pop("user", None)
+    return render_template ("login.html")
